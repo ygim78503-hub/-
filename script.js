644b6c0290ecae73c0e0592ce00635be4161e93b
ygim78503-hub <ygim78503@gmail.com>
2026-01-04 17:35:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const startBtn = document.getElementById("startBtn");
 const home = document.getElementById("home");
 const auth = document.getElementById("auth");
+const signupBtn = document.getElementById("signup");
+const signup = document.getElementById("signup");
 
 startBtn.addEventListener("click", () => {
   home.style.display = "none";
   auth.style.display = "block";
+});
+
+signupBtn.addEventListener("click", () => {
+  auth.style.display = "none";
+  signup.style.display = "block";
 });
